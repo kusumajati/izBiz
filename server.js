2411d705d82505ff4eed05328cbbdbd8ea6c1f54
kusumajati <nino.bmakj@outlook.com>
2019-05-28 02:10:45 +0700

    re-init

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const app = express();
 const port = process.env.PORT|| 5050
 
 
-// //import mongoose
-// const mongoose = require('mongoose');
+//import mongoose
+const mongoose = require('mongoose');
 
-// //import database config
-// const dbConfig = require('./config/dbServer')
+//import database config
+const dbConfig = require('./config/dbServer')
 
-// //import body parser
-// const bodyParser = require('body-parser')
+//import body parser
+const bodyParser = require('body-parser')
 
-// //configure mongoose promise
-// mongoose.Promise = global.Promise;
+//configure mongoose promise
+mongoose.Promise = global.Promise;
 
-// //connect to database
+//connect to database
 mongoose.connect(dbConfig.mongoLocalURL, {
     useNewUrlParser: true
 }) .then(() => {
@@ -25,31 +25,31 @@ mongoose.connect(dbConfig.mongoLocalURL, {
     process.exit()
 });
 
-// // parse request of content-type - application
-// app.use(bodyParser.urlencoded({extended: true}))
+// parse request of content-type - application
+app.use(bodyParser.urlencoded({extended: true}))
 
-// // parse requests of content-type - application/json
-// app.use(bodyParser.json())
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
 
-// //swagger
-// const   swaggerUi = require("swagger-ui-express"),
-//         swaggerDoc = require("./swagger.json")
-// app.use("/api/docs", swaggerUi.serve)
-// app.get("/api/docs",swaggerUi.setup(swaggerDoc))
+//swagger
+const   swaggerUi = require("swagger-ui-express"),
+        swaggerDoc = require("./swagger.json")
+app.use("/api/docs", swaggerUi.serve)
+app.get("/api/docs",swaggerUi.setup(swaggerDoc))
 
-// // define a simple route
+// define a simple route
 app.get('/', (req, res) =>{
     res.send('welcome to express')
 } )
 
-// app.get('/api/', (req, res) => res.send('this is api '))
+app.get('/api/', (req, res) => res.send('this is api '))
 
-// // require routes
-// require('./app/routes/project.route')(app);
-// require("./app/routes/user.route")(app)
+// require routes
+require('./app/routes/project.route')(app);
+require("./app/routes/user.route")(app)
 
 app.listen(port, () => {
         console.log(`you've started the server`)
     }
 )
-// module.exports = app;
+module.exports = app;
